c8d48d82d51b5b94e96cd511e3b1deb01419c195
hyuneun <dev.seojjery@gmail.com>
2025-04-10 21:58:47 +0900

    init : 초기설정

---
+++ src/main/java/com/backend/crame/CrameApplication.java
@@ -0,0 +1,13 @@
+package com.backend.crame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrameApplication.class, args);
+    }
+
+}
