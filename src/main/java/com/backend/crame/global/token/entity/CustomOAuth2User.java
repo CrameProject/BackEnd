882079442e8e5afd2ca2f388ae5db7e373eb0d02
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-03 02:33:09 +0900

    feat : Oauth user #2

---
+++ src/main/java/com/backend/crame/global/token/entity/CustomOAuth2User.java
@@ -0,0 +1,38 @@
+package com.backend.crame.global.token.entity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2User implements OAuth2User {
+
+	private final Map<String, Object> attributes;
+	private final String uuid;
+
+	public CustomOAuth2User(Map<String, Object> attributes, String uuid) {
+		this.attributes = attributes;
+		this.uuid = uuid;
+	}
+
+	public String getUserId() {
+		return uuid;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+
+	@Override
+	public String getName() {
+		return uuid;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(() -> "ROLE_USER");
+	}
+}
