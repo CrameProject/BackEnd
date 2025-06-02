4cb9004fff051da29520e9af6d215134debbb120
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-03 02:32:46 +0900

    feat : security 설정 #2

--- src/main/java/com/backend/crame/global/config/SecurityConfig.java
+++ src/main/java/com/backend/crame/global/config/SecurityConfig.java
@@ -1,4 +1,61 @@
 package com.backend.crame.global.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.util.matcher.AndServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+
+import com.backend.crame.global.token.service.JwtAuthenticationConverter;
+import com.backend.crame.global.token.service.JwtAuthenticationManager;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final CorsConfig corsConfig;
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
+		JwtAuthenticationManager jwtAuthenticationManager,
+		JwtAuthenticationConverter jwtAuthenticationConverter) {
+
+		AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(jwtAuthenticationManager);
+		jwtFilter.setSecurityContextRepository(NoOpServerSecurityContextRepository.getInstance());
+		jwtFilter.setServerAuthenticationConverter(jwtAuthenticationConverter);
+		jwtFilter.setRequiresAuthenticationMatcher(
+			new AndServerWebExchangeMatcher(
+				ServerWebExchangeMatchers.pathMatchers("/api/**"),
+				new NegatedServerWebExchangeMatcher(
+					ServerWebExchangeMatchers.pathMatchers("/api/v3/api-docs/**")
+				)
+			)
+		);
+
+		return http
+			.cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+			.csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+			.authorizeExchange(exchange -> exchange
+				.pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.pathMatchers("/", "/login/**", "/oauth2/**", "/auth/**", "/swagger-ui.html", "/swagger-ui/**",
+					"/v3/api-docs/**", "/webjars/**", "/favicon.ico", "/docs", "/health/**", "/api/v3/api-docs/**")
+				.permitAll()
+				.anyExchange().authenticated()
+			)
+			.addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+			.build();
+	}
+
 }
