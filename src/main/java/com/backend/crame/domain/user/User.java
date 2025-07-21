6472e3428e3902276ceb35ab1d461a4d7f494300
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-07-21 10:53:07 -0700

    fix : 유저오류 제거 #2

--- src/main/java/com/backend/crame/domain/user/User.java
+++ src/main/java/com/backend/crame/domain/user/User.java
@@ -1,15 +1,11 @@
 package com.backend.crame.domain.user;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import com.backend.crame.domain.terms.Terms;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
