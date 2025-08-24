a2a1a9b40b12fa4301e0d46c79b27bad1248892f
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-08-24 12:22:25 +0900

    feat : BaseTimeEntity 추가 #6

---
+++ src/main/java/com/backend/crame/global/utils/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.backend.crame.global.utils;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	@Field("created_at")
+	private Instant createdAt;
+
+	@LastModifiedDate
+	@Field("updated_at")
+	private Instant updatedAt;
+
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
