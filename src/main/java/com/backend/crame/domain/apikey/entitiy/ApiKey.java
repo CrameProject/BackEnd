447d6026a72f685b5678ca161359324d75f2b5e2
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-08-24 12:17:54 +0900

    feat : ApiKey 엔티티 추가 #6

---
+++ src/main/java/com/backend/crame/domain/apikey/entitiy/ApiKey.java
@@ -0,0 +1,40 @@
+package com.backend.crame.domain.apikey.entitiy;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "apikey")
+@Getter
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+public class ApiKey {
+	@Id
+	private String key_uuid;
+
+	private String user_uuid;
+
+	@Setter
+	private String nickname;
+
+
+	private String publicKey;
+
+	private String secretKey;
+
+	@Builder
+	private ApiKey(String key_uuid, String user_uuid, String nickname, String publicKey, String secretKey){
+		this.key_uuid = key_uuid;
+		this.user_uuid = user_uuid;
+		this.nickname = nickname;
+		this.publicKey = publicKey;
+		this.secretKey = secretKey;
+	}
+
+
+}
